a449695e40981614b026f013cebe168f21b2cdbd
oabdulr <144569041+oabdulr@users.noreply.github.com>
2024-04-06 16:21:21 -0400

    Update Stuff.js

--- Stuff.js
+++ Stuff.js
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Website</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+  </head>
+  <body>
+    <main>
+        <h1 id = "titlestuff">.</h1>  
+<h1 id = "titlestuff1">.</h1> 
+<h1 id = "titlestuff2">.</h1> 
+<h1 id = "titlestuff3">.</h1> 
+<h1 id = "titlestuff4">.</h1> 
+<h1 id = "titlestuff5">.</h1> 
+    </main>
+	<script src="main.js"></script>
+  </body>
+</html>
+
 //--------------------- Copyright Block ----------------------
 /*
 
